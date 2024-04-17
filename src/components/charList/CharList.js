cf1a06a55592b461562ac79eea0198c5a9cfc595
DmytroBeridze <moderatefast@gmail.com>
2024-04-17 15:15:52 +0300

    fixed conflict

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,4 @@
 import "./charList.scss";
-import abyss from "../../resources/img/abyss.jpg";
 import MarvelService from "../services/MarvelService";
 import React, { useEffect, useRef, useState } from "react";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -33,95 +32,88 @@ const CharList = ({ getCharId }) => {
     if (newChar.length < 9) {
       ended = true;
     }
+    setChar((char) => [...char, ...newChar]);
+    setOffset((offset) => offset + 9);
+    setLoading(false);
+    setCharEned(ended);
+  };
 
-    //  const setState(({ char, offset }) => ({
-    //       char: [...char, ...newChar],
-    //       spinner: false,
-    //       offset: offset + 9,
-    //       loading: false,
-    //       charEned: ended,
-    //     }));
-    //   };
+  const arrRef = useRef([]);
 
-    // const  onError = () => {
-    //     setState({ error: true, spinner: false });
-    //   };
+  const addStyle = (i) => {
+    arrRef.current.forEach((elem) => {
+      elem.classList.remove("char__item_selected");
+    });
+    arrRef.current[i].classList.add("char__item_selected");
+  };
 
-    const arrRefs = [];
-    const getReffs = (elem) => {
-      this.arrRefs.push(elem);
-    };
-    const addStyle = (i) => {
-      arrRefs.forEach((elem) => elem.classList.remove("char__item_selected"));
-      arrRefs[i].classList.add("char__item_selected");
-    };
-
-    const renderCard = (arr) => {
-      const card = arr.map((elem, i) => {
-        let imgStyle =
-          elem.thumbnail ===
-          "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-            ? { objectFit: "unset" }
-            : { objectFit: "cover" };
-        return (
-          // transition group
-          <CSSTransition key={elem.id} timeout={500} classNames="char__item">
-            <li
-              className="char__item"
-              key={elem.id}
-              ref={(elem) => (arrRefs.current[i] = elem)}
-              onClick={(e) => {
+  const renderCard = (arr) => {
+    const card = arr.map((elem, i) => {
+      let imgStyle =
+        elem.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+          ? { objectFit: "unset" }
+          : { objectFit: "cover" };
+      return (
+        // transition group
+        <CSSTransition key={elem.id} timeout={500} classNames="char__item">
+          <li
+            className="char__item"
+            key={elem.id}
+            ref={(elem) => (arrRef.current[i] = elem)}
+            onClick={(e) => {
+              getCharId(elem.id);
+              addStyle(i);
+            }}
+            tabIndex={1}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
                 getCharId(elem.id);
                 addStyle(i);
-              }}
-              tabIndex={1}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" || e.key === " ") {
-                  getCharId(elem.id);
-                  addStyle(i);
-                }
-              }}
-            >
-              <img src={elem.thumbnail} alt="abyss" style={imgStyle} />
-              <div className="char__name">{elem.name}</div>
-            </li>
-          </CSSTransition>
-        );
-      });
-      return (
-        <ul className="char__grid">
-          <TransitionGroup component={null}>{card}</TransitionGroup>
-        </ul>
+              }
+            }}
+          >
+            <img src={elem.thumbnail} alt="abyss" style={imgStyle} />
+            <div className="char__name">{elem.name}</div>
+          </li>
+        </CSSTransition>
       );
-    };
-
-    const err = error ? <ErrorMessage /> : null;
-    const load = spinner ? <Loader /> : null;
-    const content = !(err || load) ? renderCard(char) : null;
+    });
     return (
-      <div
-        className="char__list"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        {err}
-        {load}
-        {content}
-        <button
-          className="button button__main button__long"
-          onClick={() => this.onLoadCharacters(offset)}
-          disabled={loading}
-          style={{ display: charEned ? "none" : "block" }}
-        >
-          <div className="inner">load more</div>
-        </button>
-      </div>
+      <ul className="char__grid">
+        <TransitionGroup component={null}>{card}</TransitionGroup>
+      </ul>
     );
   };
+
+  const err = error ? <ErrorMessage /> : null;
+  const load = loader && !loading ? <Loader /> : null;
+  const content = !(err || load) ? renderCard(char) : null;
+
+  return (
+    <div
+      className="char__list"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {err}
+      {load}
+      {content}
+      <button
+        className="button button__main button__long"
+        onClick={() => onLoadCharacters(offset)}
+        disabled={loading}
+        style={{ display: charEned ? "none" : "block" }}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
 };
+
 CharList.propTypes = {
   getCharId: PropTypes.func,
 };
